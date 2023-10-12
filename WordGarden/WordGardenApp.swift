6b46393161d52b8475dabfcbffdec7e811e64bc8
David Veeneman <davidv@veeneman.com>
2023-10-12 14:00:23 -0700

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by David Veeneman on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
